dff35e0bb33fed07c7442a38a70a0d05195edca3
Sungbin_Kang <sungbink@uw.edu>
2021-10-22 14:08:06 -0700

    Schoo Catalogue finished

--- SchoolCatalogue.py
+++ SchoolCatalogue.py
@@ -29,22 +29,101 @@
 
 # Create the School Class
 class School:
-    def __init__(self, name, level, numberOfStudents):
+    def __init__(self, name, level, number_of_student):
         if not isinstance(name, str):
             raise TypeError("name must be str")
         if not (level == "primary" or level == "middle" or level == "high"):
             raise TypeError("level must be \"primary\", \"middle\" or \"high\"")
-        if not isinstance(numberOfStudents, int):
-            raise TypeError("numberOfStudent must be int")
+        if not isinstance(number_of_student, int):
+            raise TypeError("number_of_student must be int")
 
         self.name = name
         self.level = level
-        self.numberOfStudents = numberOfStudents
+        self.number_of_student = number_of_student
+
+    def get_name(self):
+        return self.name
+
+    def get_level(self):
+        return self.level
+
+    def get_student_num(self):
+        return self.number_of_student
+
+    def __repr__(self):
+        return f"A {self.level} school named {self.name} with " \
+               f"{self.number_of_student} students"
 
 
 # school test
-School("test_school", "middle", 0)
+school_test = School("School Test", "middle", 0)
+print(school_test)
 
+school_test.name = "School Test 2"
+print(school_test.get_name())
+
+school_test.level = "primary"
+print(school_test.get_level())
+
+school_test.number_of_student = 10
+print(school_test.get_student_num())
+
+print()
+
+
+# Create PrimarySchool Class
+class PrimarySchool(School):
+    def __init__(self, name, number_of_student,
+                 pickup_policy="Pick up after 3pm"):
+        super().__init__(name, "primary", number_of_student)
+
+        if not isinstance(pickup_policy, str):
+            raise TypeError("pickup_policy must be str")
+        self.pickup_policy = pickup_policy
+
+    def get_pickup_policy(self):
+        return self.pickup_policy
+
+    def __repr__(self):
+        return super().__repr__() + f"\nPickup policy: {self.pickup_policy}"
+
+
+# primary school test
+primary_school_test = PrimarySchool("Primary School Test", 0)
+print(primary_school_test)
+
+primary_school_test.pickup_policy = "Pick up After 4pm"
+print(primary_school_test.get_pickup_policy())
+
+print()
+
+
+# Create HighSchool Class
+class HighSchool(School):
+    def __init__(self, name, number_of_student, sports_teams=None):
+        super().__init__(name, "high", number_of_student)
+
+        if sports_teams is None:
+            sports_teams = []
+        self.sports_teams = sports_teams
+
+    def get_sports_teams(self):
+        return self.sports_teams
+
+    def add_sports_team(self, team):
+        self.sports_teams.append(team)
+
+    def __repr__(self):
+        return super().__repr__() + f"\nSports Teams: {self.sports_teams}"
+
+
+# high school test
+high_school_test = HighSchool("High School Test", 0)
+print(high_school_test)
+
+high_school_test.add_sports_team("team1")
+high_school_test.add_sports_team("team2")
+print(high_school_test.get_sports_teams())
 
 
 # empty line
